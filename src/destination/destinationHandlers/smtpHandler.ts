042f94ea0c61cf518301b880aeeab38f83f46e33
adi6859 <aditya.ar1909@gmail.com>
2024-05-20 14:59:25 +0530

    poc code

--- src/destination/destinationHandlers/smtpHandler.ts
+++ src/destination/destinationHandlers/smtpHandler.ts
@@ -22,6 +22,7 @@ export class SMTPService implements Handler {
         port: string
         host: string
         auth_user: string
+        auth_type:string
         auth_password: string
         from_email: string
     }
@@ -62,6 +63,7 @@ export class SMTPService implements Handler {
                 port: config['port'],
                 host: config['host'],
                 auth_user: config['auth_user'],
+                auth_type: config["auth_type"],
                 auth_password: config['auth_password'],
                 from_email: config['from_email']
             }
@@ -90,21 +92,53 @@ export class SMTPService implements Handler {
     }
 
     private preparePaylodAndSend(event: Event, smtpTemplate: NotificationTemplates, setting: NotificationSettings, p: string){
+        const smtpConfig = this.smtpConfig;
+        // const authMethod = 'PLAIN';
+
+        // Create the email provider configuration
+        let emailProviderConfig: any = {
+          type: "smtp",
+          port: smtpConfig["port"],
+          host: smtpConfig["host"],
+        //   authMethod: 'PLAIN',
+        //   tls: {
+        //     // do not fail on invalid certs
+        //     rejectUnauthorized: false,
+        //   },
+        };
+
+        // Conditionally add the auth object if authMethod is not 0
+        if (smtpConfig["auth_user"] !== '') {
+          emailProviderConfig.auth = {
+            user: smtpConfig["auth_user"],
+            pass: smtpConfig["auth_password"],
+          };
+        }
+
+        // Create the NotifmeSdk instance
         let sdk: NotifmeSdk = new NotifmeSdk({
-            channels: {
-                email: {
-                    providers: [{
-                        type: 'smtp',
-                        port: this.smtpConfig['port'],
-                        host: this.smtpConfig['host'],
-                        auth:{
-                            user: this.smtpConfig['auth_user'],
-                            pass: this.smtpConfig['auth_password'],
-                        }
-                    }]
-                }
-            }
+          channels: {
+            email: {
+              providers: [emailProviderConfig],
+            },
+          },
         });
+        // let sdk: NotifmeSdk = new NotifmeSdk({
+        //     channels: {
+        //         email: {
+        //             providers: [{
+        //                 type: 'smtp',
+        //                 port: 1025,
+        //                 host: "localhost",
+        //                 authMethod:0,
+        //                 auth:{
+        //                     // user: this.smtpConfig['auth_user'],
+        //                     // pass: this.smtpConfig['auth_password'],
+        //                 }
+        //             }]
+        //         }
+        //     }
+        // });
         event.payload['fromEmail'] = this.smtpConfig['from_email']
         let engine = new Engine();
         // let options = { allowUndefinedFacts: true }
