23a15d18329ac8118d582c8dc7481a7003e2f6c4
komalreddy3 <koukuntlakomalreddy3@gmail.com>
2024-06-18 15:28:22 +0530

    fix: max ack pending

--- src/pubSub/pubSub.ts
+++ src/pubSub/pubSub.ts
@@ -63,7 +63,7 @@ export class PubSubServiceImpl implements PubSubService {
             deliver_group:queueName,
             ack_policy:AckPolicy.Explicit,
             deliver_policy:DeliverPolicy.Last,
-
+           max_ack_pending:1,
         }).bindStream(streamName).callback((err, msg) => {
             try {
                 const msgString = getJsonString(msg.data)
@@ -99,6 +99,7 @@ export class PubSubServiceImpl implements PubSubService {
                     deliver_group:queueName,
                     ack_policy:AckPolicy.Explicit,
                     deliver_policy:DeliverPolicy.Last,
+                    max_ack_pending:1,
                 })
                 this.logger.info("consumer added successfully")
             } catch (err) {
