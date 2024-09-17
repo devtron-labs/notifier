d759984f3949deb5227c48bb0c1719b35cf980af
Gireesh Naidu <111440205+gireesh-naidu@users.noreply.github.com>
2024-09-17 13:19:47 +0530

    feat: add cluster filter in notification setting (#73)
    * feat: add cluster filtering for configuring the notifications
    
    * test fix
    
    * fix: legacy query fix
    
    * fix: query fix for all prod/non prod environments
    
    * app and env combination case missing in legacy query
    
    * app and env combination case missing in legacy query
    
    * chore: comments added
    
    * chore: bump version for axios, braces and notifme-sdk
    
    ---------
    
    Co-authored-by: Eshank Vaish <eshank@devtron.ai>

--- src/notification/service/notificationService.ts
+++ src/notification/service/notificationService.ts
@@ -159,7 +159,7 @@ class NotificationService {
                 throw new CustomError("Event is not valid", 400)
             }
 
-            const settingsResults=await this.notificationSettingsRepository.findByEventSource(event.pipelineType, event.pipelineId, event.eventTypeId, event.appId, event.envId, event.teamId);
+            const settingsResults=await this.notificationSettingsRepository.findByEventSource(event.pipelineType, event.pipelineId, event.eventTypeId, event.appId, event.envId, event.teamId, event.clusterId, event.isProdEnv);
                 this.logger.info('notificationSettingsRepository.findByEventSource')
                 if (!settingsResults || settingsResults.length == 0) {
                     this.logger.info("no notification settings found for event " + event.correlationId);
@@ -253,7 +253,8 @@ class Event {
     appId: number
     envId: number
     teamId: number
+    clusterId: number
+    isProdEnv: boolean
     baseUrl?: string
 }
-
 export {NotificationService, Event}
