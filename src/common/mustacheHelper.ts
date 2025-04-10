f333e76e04c8e89c4afac4268e5674bd9a7b5e17
Prakash Kumar <prakash.kumar@devtron.ai>
2025-04-10 14:14:00 +0530

    triggered without approval warning msg

--- src/common/mustacheHelper.ts
+++ src/common/mustacheHelper.ts
@@ -158,6 +158,8 @@ export class MustacheHelper {
                 deploymentHistoryLink: deploymentHistoryLink,
                 deploymentWindowComment: event.payload.timeWindowComment ?? '',
                 deploymentWindowCommentStyle: event.payload.timeWindowComment ? 'block' : 'none',
+                triggeredWithoutApproval: event.isDeploymentDoneWithoutApproval ? "Deployment triggered without approval" : '',
+                triggeredWithoutApprovalStyle: event.isDeploymentDoneWithoutApproval ? 'block' : 'none',
             }
         }
         else if (event.eventTypeId===EVENT_TYPE.Approval){
--- src/common/types.ts
+++ src/common/types.ts
@@ -134,6 +134,8 @@ export interface ParsedCDEvent {
     dockerImg: string;
     deploymentWindowComment?: string;
     deploymentWindowCommentStyle?: string;
+    triggeredWithoutApproval?: string;
+    triggeredWithoutApprovalStyle?: string;
 }
 export enum EVENT_TYPE {
     Trigger = 1,
